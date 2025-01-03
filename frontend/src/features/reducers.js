469d541743a7a92f335f33681e6e9dd4ce8ba937
Elmoatassimm <mouatassemben2004@gmail.com>
2025-01-03 22:04:11 +0100

    Stop tracking frontend folder

--- frontend/src/features/reducers.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import projectReducer from './project/projectSlice';
-
-const rootReducer = combineReducers({
-  projects: projectReducer,
-  // Add other reducers here as the application grows
-});
-
-export default rootReducer;
